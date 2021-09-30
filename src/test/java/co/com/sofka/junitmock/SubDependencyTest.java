147b79f87236ed9ac217036dde9fb75d93a7530b
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-09-29 20:51:30 -0500

    se supero la prueba con excepcion como retorno

--- src/test/java/co/com/sofka/junitmock/SubDependencyTest.java
+++ src/test/java/co/com/sofka/junitmock/SubDependencyTest.java
@@ -1,4 +1,39 @@
+package co.com.sofka.junitmock;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
 class SubDependencyTest {
-  
+
+    @Mock
+    private Dependency dependency;
+
+    @BeforeEach
+    void setUpMock() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testDependency(){
+        when(dependency.getClassName()).thenReturn("hi there");
+
+        assertEquals("hi there", dependency.getClassName());
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void testException() {
+        when(dependency.getClassName()).thenThrow(IllegalArgumentException.class);
+
+        dependency.getClassName();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
 }
